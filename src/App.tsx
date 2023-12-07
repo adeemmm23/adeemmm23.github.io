cb8edb5e1cb5bbedaf772e2f7efe813680a563c8
Adem <112782893+adeemmm23@users.noreply.github.com>
2023-12-07 17:40:40 +0100

    Reformat page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import Landing from "@/pages/landing.tsx";
 import SignInPage from "@/pages/signIn/sign-in-page.tsx";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import { ThemeProvider } from "@/components/theme-provider"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "@/components/theme-provider";
 import SignUpPage from "@/pages/signUp/sign-up-page.tsx";
 
 function App() {
-
   return (
     <>
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <main className={"h-screen"}>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path={"/"} Component={Landing} />
-                        <Route path={"/sign-in"} Component={SignInPage} />
-                        <Route path={"/sign-up"} Component={SignUpPage} />
-                    </Routes>
-                </BrowserRouter>
-            </main>
-        </ThemeProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <main className={"h-screen"}>
+          <BrowserRouter>
+            <Routes>
+              <Route path={"/"} Component={Landing} />
+              <Route path={"/sign-in"} Component={SignInPage} />
+              <Route path={"/sign-up"} Component={SignUpPage} />
+            </Routes>
+          </BrowserRouter>
+        </main>
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
