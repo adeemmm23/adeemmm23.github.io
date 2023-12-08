5ed101be14cf88cb35ed2aa228ab27a1b93422f5
Adem <112782893+adeemmm23@users.noreply.github.com>
2023-12-08 06:47:55 +0100

    Add new files and update configurations

---
+++ src/pages/dashboard/dashboard.tsx
@@ -0,0 +1,153 @@
+import { z } from "zod";
+import React, { useEffect, useState } from "react";
+import { columns } from "./components/columns";
+import { DataTable } from "./components/data-table";
+import { UserNav } from "./components/user-nav";
+import { taskSchema } from "./data/schema";
+import tasksData from "./data/tasks.json";
+
+// import Nuit2023 from "@/assets/logo/nuit_2023.svg";
+// import Nuit2022 from "@/assets/logo/nuit_2022.svg";
+// import Nuit2021 from "@/assets/logo/nuit_2021.svg";
+// import Nuit2019 from "@/assets/logo/nuit_2019.svg";
+// import Nuit2018 from "@/assets/logo/nuit_2018.svg";
+// import Nuit2017 from "@/assets/logo/nuit_2017.svg";
+// import Nuit2016 from "@/assets/logo/nuit_2016.svg";
+// import Nuit2015 from "@/assets/logo/nuit_2015.svg";
+// import Nuit2014 from "@/assets/logo/nuit_2014.svg";
+// import Nuit2013 from "@/assets/logo/nuit_2013.svg";
+// import Nuit2012 from "@/assets/logo/nuit_2012.svg";
+// import Nuit2011 from "@/assets/logo/nuit_2011.svg";
+// import Nuit2010 from "@/assets/logo/nuit_2010.svg";
+// import Nuit2009 from "@/assets/logo/nuit_2009.svg";
+// import Nuit2008 from "@/assets/logo/nuit_2008.svg";
+// import Nuit2007 from "@/assets/logo/nuit_2007.svg";
+
+import Nuit2023 from "../../../public/logo/nuit_2023.svg";
+import Nuit2022 from "../../../public/logo/nuit_2022.svg";
+import Nuit2021 from "../../../public/logo/nuit_2021.svg";
+import Nuit2019 from "../../../public/logo/nuit_2019.svg";
+import Nuit2018 from "../../../public/logo/nuit_2018.svg";
+import Nuit2017 from "../../../public/logo/nuit_2017.svg";
+import Nuit2016 from "../../../public/logo/nuit_2016.svg";
+import Nuit2015 from "../../../public/logo/nuit_2015.svg";
+import Nuit2014 from "../../../public/logo/nuit_2014.svg";
+import Nuit2013 from "../../../public/logo/nuit_2013.svg";
+import Nuit2012 from "../../../public/logo/nuit_2012.svg";
+import Nuit2011 from "../../../public/logo/nuit_2011.svg";
+import Nuit2010 from "../../../public/logo/nuit_2010.svg";
+import Nuit2009 from "../../../public/logo/nuit_2009.svg";
+import Nuit2008 from "../../../public/logo/nuit_2008.svg";
+import Nuit2007 from "../../../public/logo/nuit_2007.svg";
+
+type Task = {
+  id: string;
+  title: string;
+  status: string;
+  label: string;
+  priority: string;
+};
+
+export let theme = "nuit2022";
+
+const themes = [
+  "nuit2007",
+  "nuit2008",
+  "nuit2009",
+  "nuit2010",
+  "nuit2011",
+  "nuit2012",
+  "nuit2013",
+  "nuit2014",
+  "nuit2015",
+  "nuit2016",
+  "nuit2017",
+  "nuit2018",
+  "nuit2019",
+  "nuit2021",
+  "nuit2022",
+  "nuit2023",
+];
+interface Logo {
+  [key: string]: string;
+}
+
+const logo: Logo = {
+  nuit2023: Nuit2023,
+  nuit2022: Nuit2022,
+  nuit2021: Nuit2021,
+  nuit2019: Nuit2019,
+  nuit2018: Nuit2018,
+  nuit2017: Nuit2017,
+  nuit2016: Nuit2016,
+  nuit2015: Nuit2015,
+  nuit2014: Nuit2014,
+  nuit2013: Nuit2013,
+  nuit2012: Nuit2012,
+  nuit2011: Nuit2011,
+  nuit2010: Nuit2010,
+  nuit2009: Nuit2009,
+  nuit2008: Nuit2008,
+  nuit2007: Nuit2007,
+};
+
+interface ThemeNames {
+  [key: string]: string;
+}
+
+const themeNames: ThemeNames = {
+  nuit2023: "Nuit de l'info 2023",
+  nuit2022: "Nuit de l'info 2022",
+  nuit2021: "Nuit de l'info 2021",
+  nuit2019: "Nuit de l'info 2019",
+  nuit2018: "Nuit de l'info 2018",
+  nuit2017: "Nuit de l'info 2017",
+  nuit2016: "Nuit de l'info 2016",
+  nuit2015: "Nuit de l'info 2015",
+  nuit2014: "Nuit de l'info 2014",
+  nuit2013: "Nuit de l'info 2013",
+  nuit2012: "Nuit de l'info 2012",
+  nuit2011: "Nuit de l'info 2011",
+  nuit2010: "Nuit de l'info 2010",
+  nuit2009: "Nuit de l'info 2009",
+  nuit2008: "Nuit de l'info 2008",
+  nuit2007: "Nuit de l'info 2007",
+};
+
+export default function Dashboard() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    try {
+      theme = themes[Math.floor(Math.random() * themes.length)];
+      const parsedTasks = z.array(taskSchema).parse(tasksData);
+      setTasks(parsedTasks);
+    } catch (error) {
+      console.error("Error parsing tasks:", error);
+    }
+  }, []);
+
+  return (
+    <>
+      <div className="hidden h-screen flex-1 flex-col space-y-8 p-10 md:flex font-Inter">
+        <div className="flex items-center justify-between space-y-2">
+          <div className="flex items-center justify-start gap-4">
+            <img src={logo[theme]} alt="Nuit Logo" className="w-16" />
+            <div>
+              <h2 className="text-2xl font-bold tracking-tight">
+                {"Bon retour à " + themeNames[theme]}
+              </h2>
+              <p className="text-muted-foreground">
+                Here&apos;s a list of your tasks for this month!
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center space-x-2">
+            <UserNav />
+          </div>
+        </div>
+        <DataTable data={tasks} columns={columns} />
+      </div>
+    </>
+  );
+}
---
+++ src/pages/signUp/sign-up-form.tsx
@@ -0,0 +1,145 @@
+import React, { useEffect, useState } from "react";
+import { cn } from "@/lib/utils.ts";
+import { Label } from "@/components/ui/label.tsx";
+import { Input } from "@/components/ui/input.tsx";
+import { Button } from "@/components/ui/button.tsx";
+import { Loader2, Github } from "lucide-react";
+import { useTheme } from "@/components/theme-provider";
+import { Navigate, useNavigate } from "react-router-dom";
+
+type ButtonVariant =
+  | "default"
+  | "link"
+  | "transparent"
+  | "transparentDark"
+  | "destructive"
+  | "outline"
+  | "secondary"
+  | "ghost"
+  | null
+  | undefined;
+
+interface SignInProps extends React.HTMLAttributes<HTMLDivElement> {}
+const SignUpForm = ({ className, ...props }: SignInProps) => {
+  const { theme } = useTheme();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [buttonVariant, setButtonVariant] = useState<ButtonVariant>("default");
+  const navigate = useNavigate();
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+      navigate("/dashboard");
+    }, 3000);
+  }
+
+  useEffect(() => {
+    theme == "dark"
+      ? setButtonVariant("transparent")
+      : setButtonVariant("transparentDark");
+  }, [theme]);
+
+  return (
+    <div className={cn("grid gap-6", className)} {...props}>
+      <form onSubmit={onSubmit}>
+        <div className="grid gap-3">
+          <div className={"grid-cols-2 grid gap-x-2 w-full"}>
+            <div>
+              <Label className="sr-only" htmlFor="nom">
+                Nom
+              </Label>
+              <Input
+                id="nom"
+                placeholder="nom"
+                type="text"
+                autoCapitalize="none"
+                autoCorrect="off"
+                disabled={isLoading}
+                className={"rounded-full"}
+              />
+            </div>
+            <div>
+              <Label className="sr-only" htmlFor="prenom">
+                prenom
+              </Label>
+              <Input
+                id="prenom"
+                placeholder="prenom"
+                type="text"
+                autoCapitalize="none"
+                autoCorrect="off"
+                disabled={isLoading}
+                className={"rounded-full"}
+              />
+            </div>
+          </div>
+
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              E-mail
+            </Label>
+            <Input
+              id="email"
+              placeholder="nom@example.com"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+              className={"rounded-full"}
+            />
+          </div>
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="Password">
+              Mot de passe
+            </Label>
+            <Input
+              id="Password"
+              type="Password"
+              placeholder={"mot de passe"}
+              autoCapitalize="none"
+              autoCorrect="off"
+              disabled={isLoading}
+              className={"rounded-full"}
+            />
+          </div>
+          <Button
+            variant={buttonVariant}
+            className={"rounded-full"}
+            disabled={isLoading}
+          >
+            {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+            Créer un compte avec le courriel
+          </Button>
+        </div>
+      </form>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Ou continuer avec
+          </span>
+        </div>
+      </div>
+      <Button
+        variant="outline"
+        className={"rounded-full"}
+        type="button"
+        disabled={isLoading}
+      >
+        {isLoading ? (
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+        ) : (
+          <Github className="mr-2 h-4 w-4" />
+        )}{" "}
+        Github
+      </Button>
+    </div>
+  );
+};
+
+export default SignUpForm;
